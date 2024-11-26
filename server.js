0a31ca2803b01aa4bc77711ae7adbcf3345010ab
Yodahe Teshome <jodahe2001@gmail.com>
2024-11-26 23:49:15 +0300

    Connection to Mongo DB

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+const booksRoutes = require('./routes/books');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+
+connectDB();
+
+
+app.use('/books', booksRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
