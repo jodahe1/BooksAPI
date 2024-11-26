0ca720772ae094947e8d23c3aa2249d28eb01a46
Yodahe Teshome <jodahe2001@gmail.com>
2024-11-27 00:26:17 +0300

    Re-depolying

--- server.js
+++ server.js
@@ -6,14 +6,16 @@ const booksRoutes = require('./routes/books');
 
 const app = express();
 
-
+// Middleware
 app.use(bodyParser.json());
 
-
+// Connect to database
 connectDB();
 
-
+// Routes
 app.use('/books', booksRoutes);
 
+// Use PORT from environment variable or default to 3000
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
