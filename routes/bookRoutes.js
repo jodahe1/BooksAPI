71b34ec6cfb78f65830e3c23a6270399614f076c
Yodahe Teshome <jodahe2001@gmail.com>
2024-12-06 19:15:22 +0300

    Deploying

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -12,22 +12,19 @@ const roleMiddleware = require('../middlewares/roleMiddleware');
 
 const router = express.Router();
 
-// Admin-only route: Get all books
+
 router.get('/all', authMiddleware, roleMiddleware('admin'), getAllBooks);
 
-// General route: Get a specific book by ID
 router.get('/:id', authMiddleware, getBookById);
 
-// General route: Get books accessible to the user
 router.get('/', authMiddleware, getBooks);
 
-// General route: Add a new book (User or Admin)
+
 router.post('/', authMiddleware, createBook);
 
-// General route: Update a book by ID (User or Admin)
 router.put('/:id', authMiddleware, updateBookById);
 
-// Admin-only route: Delete a book by ID
+
 router.delete('/:id', authMiddleware, roleMiddleware('admin'), deleteBookById);
 
 module.exports = router;
