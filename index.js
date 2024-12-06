4086787e24716b2d4b2940a49cb9e34c09a47897
Yodahe Teshome <jodahe2001@gmail.com>
2024-12-06 18:15:27 +0300

    poert number changed

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use('/auth', authRoutes);
 app.use('/books', bookRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
